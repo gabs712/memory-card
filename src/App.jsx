12e9e6b6db6a5eff8eeef7ba7ab9d148abc99744
gabs <gabs712.g@gmail.com>
2025-02-17 22:20:13 -0300

    Cleanup container

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Game from './components/Game'
 
 export default function App() {
   return (
-    <Container className="bg-sky-600">
+    <Container>
       <Game />
     </Container>
   )
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
-export default function Container({ children, className = '' }) {
+export default function Container({ children }) {
   return (
-    <div className={`min-h-svh w-full ${className}`}>
+    <div className={`min-h-svh w-full bg-sky-600`}>
       <div className="max-w-7xl relative mx-auto">{children}</div>
     </div>
   )
